d5f111fec7788cb5fc346e6d6291bca219bb5202
Federico Mestrone <federico@moodsdesign.com>
2012-07-07 04:49:14 -0700

    Continue port to 1.8.6

--- languages/en.php
+++ languages/en.php
@@ -1,41 +1,35 @@
 <?php
-	/**
-	 * API Admin language pack.
-	 * 
-	 * @package ElggAPIAdmin
-	 * @author Curverider Ltd
-	 * @copyright Curverider Ltd 2008-2010
-	 * @link http://elgg.com/
-	 */
+/**
+ * Elgg API Admin
+ * 
+ * @package ElggAPIAdmin
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * 
+ * Upgraded to Elgg 1.8 (tested on 1.8.6)
+ * @author Federico Mestrone
+ * @copyright Curverider Ltd 2011 and Moodsdesign Ltd 2012
+ * @link http://www.elgg.org
+*/
 
+$english = array(
+	'admin:apiadmin' => 'API Administration',
 
-	$english = array(
-	
-		/**
-		 * Menu items and titles
-		 */
-	
-			'admin:apiadmin' => 'API Administration',
-	
-	
-			'apiadmin:keyrevoked' => 'API Key revoked',
-			'apiadmin:keynotrevoked' => 'API Key could not be revoked',
-			'apiadmin:generated' => 'API Key successfully generated',
-	
-			'apiadmin:yourref' => 'Your reference',
-			'apiadmin:generate' => 'Generate a new keypair',
-	
-			'apiadmin:noreference' => 'You must provide a reference for your new key.',
-			'apiadmin:generationfail' => 'There was a problem generating the new keypair',
-			'apiadmin:generated' => 'New API keypair generated successfully',
-	
-			'apiadmin:revoke' => 'Revoke key',
-			'apiadmin:public' => 'Public',
-			'apiadmin:private' => 'Private',
+	'apiadmin:keyrevoked' => 'API Key revoked',
+	'apiadmin:keynotrevoked' => 'API Key could not be revoked',
+	'apiadmin:generated' => 'API Key successfully generated',
 
-	
-			'item:object:api_key' => 'API Keys',
-	);
-					
-	add_translation("en",$english);
-?>
+	'apiadmin:yourref' => 'Your reference',
+	'apiadmin:generate' => 'Generate a new keypair',
+
+	'apiadmin:noreference' => 'You must provide a reference for your new key.',
+	'apiadmin:generationfail' => 'There was a problem generating the new keypair',
+	'apiadmin:generated' => 'New API keypair generated successfully',
+
+	'apiadmin:revoke' => 'Revoke key',
+	'apiadmin:public' => 'Public',
+	'apiadmin:private' => 'Private',
+
+	'item:object:api_key' => 'API Keys'
+);
+
+add_translation('en', $english);
--- views/default/object/api_key.php
+++ views/default/object/api_key.php
@@ -1,38 +1,38 @@
 <?php
-	global $CONFIG;
-	
-	$entity = $vars['entity'];
-	
-	$icon = elgg_view(
-			'graphics/icon', array(
-			'entity' => $entity,
-			'size' => 'small',
-		  )
-		);
+global $CONFIG;
 
-		
-	$public_label = elgg_echo('apiadmin:public');
-	$private_label = elgg_echo('apiadmin:private');
-	$revoke_label = elgg_echo('apiadmin:revoke');
-	
-	$ts = time();
-	$token = generate_action_token($ts);
-		
-		
-	$info  = "<div class=\"contentWrapper\">";
-	$info .= "<p><b>{$entity->title}</b> ";
-	$info .= "<a href=\"{$CONFIG->url}apiadmin/revokekey?keyid={$entity->guid}&__elgg_token=$token&__elgg_ts=$ts\">$revoke_label</a>";
-	$info .= "</p></div>";
-	$info .= "<div><p><b>$public_label:</b> {$entity->public}<br />";
+$entity = $vars['entity'];
 
-	if ( elgg_is_admin_logged_in() ) {
-		// Only show secret portion to admins
-		
-		// Fetch key
-		$keypair = get_api_user($CONFIG->site_id, $entity->public);
-	
-		$info .= "<b>$private_label:</b> {$keypair->secret}"; 
-	}
-	$info .= "</p></div>";
-	
-	echo elgg_view_listing($icon, $info);
+$icon = elgg_view(
+	'graphics/icon', array(
+		'entity' => $entity,
+		'size' => 'small',
+	)
+);
+
+
+$public_label = elgg_echo('apiadmin:public');
+$private_label = elgg_echo('apiadmin:private');
+$revoke_label = elgg_echo('apiadmin:revoke');
+
+$ts = time();
+$token = generate_action_token($ts);
+
+
+$info  = "<div class=\"contentWrapper\">";
+$info .= "<p><b>{$entity->title}</b> ";
+$info .= "[<a href=\"{$CONFIG->url}apiadmin/revokekey?keyid={$entity->guid}&__elgg_token=$token&__elgg_ts=$ts\">$revoke_label</a>]";
+$info .= "</p></div>";
+$info .= "<div><p><b>$public_label:</b> {$entity->public}<br />";
+
+if ( elgg_is_admin_logged_in() ) {
+	// Only show secret portion to admins
+
+	// Fetch key
+	$keypair = get_api_user($CONFIG->site_id, $entity->public);
+
+	$info .= "<b>$private_label:</b> {$keypair->secret}"; 
+}
+$info .= "</p></div>";
+
+echo elgg_view_listing($icon, $info);
