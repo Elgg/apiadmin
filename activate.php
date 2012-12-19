584f6f3cd449dc308d790cf415552ab00731a9de
Federico Mestrone <fmestrone@gmail.com>
2012-12-19 07:58:22 +0200

    Completed issue ELGGAPIADM-3: Added the Browscap library to the plugin

--- activate.php
+++ activate.php
@@ -4,5 +4,9 @@ global $CONFIG;
 // create the tables for API stats
 run_sql_script("{$CONFIG->path}mod/apiadmin/schema/mysql.sql");
 
+if ( !file_exists($browscapdir = elgg_get_data_path() . 'phpbrowscap') ) {
+    mkdir($browscapdir);
+}
+
 // plugin can be activated
 return true;
